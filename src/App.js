bc11665cb46c67912ca43d491d2b408b40f8941b
VaishnaviGhatol <vaishughatol10oct@gmail.com>
2024-04-13 16:23:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import CreateArea from './Components/CreateArea';
+import Notes from './Components/Notes';
+import Header from './Components/Header';
+import { Footer } from './Components/Footer';
+import { useState } from 'react';
+import './Components/External.css';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNotes){
+    setNotes(prevNotes =>{
+      return[
+        ...prevNotes,
+        newNotes
+      ]
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+     <CreateArea onAdd = {addNote}/>
+      {notes.map((eachNote, index)=>{
+        return(
+          <Notes
+          key = {index}
+          id = {index}
+          title = {eachNote.title}
+          content = {eachNote.content}
+
+          />
+        )
+      })}
+     
+   
+     <Footer />
     </div>
   );
 }
